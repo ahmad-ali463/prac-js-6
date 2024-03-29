888b4fa3453470749d934f3b2de49579a9149f4a
Ahmad Ali <spyali463@gmail.com>
2024-03-29 20:40:14 +0500

    revise

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    var paragraph = document.getElementById("demo");
+    paragraph.innerHTML = "Text changed!";
+});
+
+
+
+
+document.getElementById("text").onclick = function(){
+    document.getElementById("ali").style.color = "blue"
+}
+document.getElementById("ali").innerHTML = "<span> This is a span</span> "
